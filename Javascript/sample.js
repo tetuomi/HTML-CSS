f3985cfeae52290de859bd0a3ab375959838afa8
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-11 23:41:43 +0900

    make better deleting target

--- Javascript/sample.js
+++ Javascript/sample.js
@@ -3,6 +3,6 @@ function OnLinkClick(){
     var hour=time.getHours();
     var minute=time.getMinutes();
     var second=time.getSeconds();
-    target=document.getElementById("output");
-    target.innerHTML=hour+"時"+minute+"分"+second+"秒に読破しましたね♡";
+    document.getElementById("output").innerHTML=
+        hour+"時"+minute+"分"+second+"秒に読破しましたね♡";
 };
