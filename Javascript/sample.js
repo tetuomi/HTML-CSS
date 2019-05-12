5c4183da871b6d644ae98e3444a9a6c9ecc16037
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-13 00:16:33 +0900

    auto scroll

--- Javascript/sample.js
+++ Javascript/sample.js
@@ -6,3 +6,13 @@ function OnLinkClick(){
     document.getElementById("output").innerHTML=
         hour+"時"+minute+"分"+second+"秒に読破しましたね♡";
 };
+
+function Scroll(){
+    var a = document.documentElement;
+    var y = a.scrollHeight - a.clientHeight;
+    window.scroll(0, y);
+
+    // scrollHeight：ページの高さ
+    // clientHeight：ブラウザの高さ
+    // window.scroll(水平方向,垂直方向)
+};
