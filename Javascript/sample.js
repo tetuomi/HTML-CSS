42e1ddd70a8fb22f9a04c8729a6accc2bb1c8aa6
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-15 16:12:42 +0900

    update per minute

--- Javascript/sample.js
+++ Javascript/sample.js
@@ -1,13 +1,22 @@
 var count=0;
 
-function OnLinkClick(){
+function show_time(id){
     var time=new Date();
+
     var hour=time.getHours();
     var minute=time.getMinutes();
-    var second=time.getSeconds();
-    document.getElementById("output").innerHTML=
-        hour+"時"+minute+"分"+second+"秒に読破しましたね♡";
+  //  var second=time.getSeconds();
+    document.getElementById(id).innerHTML=
+        hour+"時"+minute+"分"; // +second+"秒";
+/*
+    document.getElementById(id).innerHTML=
+        time.toLocaleTimeString();
+*/
 };
+
+window.onload = function(){
+    show_time("clock");
+}
 
 function Scroll(){
     var a = document.documentElement;
@@ -35,6 +44,11 @@ function delete_button(){
     div.setAttribute("style","display:none");
 };
 
+function show_text(){
+    var text=document.form1.textarea1.value;
+    document.getElementById("text").innerHTML=text+"と書き込みましたね♡";
+};
+
 function hit_count(){
     count++
 };
@@ -43,7 +57,8 @@ function show_result(){
     var box=document.getElementById("result");
     box.innerHTML="あなたのクリックパワーは　"+count+" です。<br>";
 
-    if(count>=25)box.innerHTML+="師匠と呼んでもいいですか？";
+    if(count>=42)box.innerHTML+="ゴッドフィンガー";
+    else if(count>=35)box.innerHTML+="あはん。上にはえのき師匠がいるぞ";
     else if(count>=20)box.innerHTML+="テクニシャンにきみはなる!";
     else{box.innerHTML+="見てられん";}
 };
