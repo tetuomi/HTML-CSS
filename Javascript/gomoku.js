334cbe7efb7ac99d6425817d4a3c985411c7d7e1
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-16 23:02:50 +0900

    make better

--- Javascript/gomoku.js
+++ Javascript/gomoku.js
@@ -1,15 +1,15 @@
 const board = new Array(81);
 var turn = 0; // 0:BLACK  1:WHITE
 
-for(var i = 0;i < 81;i++){ //無駄なことしてるが、undefinedが入ってしまいできなっかた
-    if(i%9 == 0) board[i] = "<tr><td bgcolor='#CC9966' id='No"+i+"' onclick='getID(this)'></td>";
-    else if(i%9 == 8) board[i] = "<td bgcolor='#CC9966'id='No"+i+"' onclick='getID(this);'></td></tr>";
-    else board[i] = "<td bgcolor='#CC9966' id='No"+i+"' onclick='getID(this);'></td>";
+for(var i = 0;i < 81;i++){
+    board[i] = i;
 }
 document.write("<center>");
 document.write("<table rules='all'>");
 for(var i = 0;i < 81;i++){
-    document.write(board[i]);
+    if(i%9 == 0) document.write("<tr>");
+    document.write("<td bgcolor='#CC9966' class='none' id='No",board[i],"' onclick='getID(this);'></td>");
+    if(i%9 == 8) document.write("</tr>");
 }
 document.write("</table>");
 document.write("</center>");
@@ -17,6 +17,30 @@ document.write("</center>");
 function getID(element) {
     var id = element.id;
     var position = document.getElementById(id);
-    position.innerHTML="<div class='circle"+turn+"'></div>";
+       console.log(position.className)
+    position.className="circle"+turn;
     turn ^= 1;
+ 
 }
+/*
+function length(element){
+    const directions = new Array(-9,-8,1,10);
+    var position = document.getElementById(element.id);
+    var Cname = position.innerHTML.className;
+    var num = 0;
+    var x;
+    for(var i = 0;i < 4;i++){
+        
+        for(var j = 0;j < 5;j++){
+            if((position + directions[i]*j) < 0 || (position + directions[i]*j) > 80) break;
+            x = document.getElementById("No" + (position + directions[i]*j)).innerHTML.className;
+            if(x == "none")break;
+            else if(x == Cname) num++;
+        }
+        for(var j = 0;j < 5;j++){
+            
+        }
+    }
+}
+
+*/
