d89c000b387ffc07ffc0a5dbdd5c580ebde3e386
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-17 00:22:49 +0900

    add judge function

--- Javascript/gomoku.js
+++ Javascript/gomoku.js
@@ -1,10 +1,10 @@
 var turn = 0; // 0:BLACK  1:WHITE
-
+document.write("<center><h1>");
 document.write("<center>");
 document.write("<table rules='all'>");
 for(var i = 0;i < 81;i++){
     if(i%9 == 0) document.write("<tr>");
-    document.write("<td bgcolor='#CC9966' class='none' id='",i,"' onclick='getID(this);'></td>");
+    document.write("<td bgcolor='#CC9966' class='none' id='",i,"' onclick='getID(this);judge(length(this));'></td>");
     if(i%9 == 8) document.write("</tr>");
 }
 document.write("</table>");
@@ -14,26 +14,41 @@ function getID(element) {
     var id = element.id;
     var position = document.getElementById(id);
     console.log(position)
-    position.className="circle"+turn;
+    position.className = "circle" + turn;
     turn ^= 1;
 }
-/*
+
 function length(element){
     const directions = new Array(-9,-8,1,10);
     var position = document.getElementById(element.id);
     var Cname = position.className;
-    var num = 0;
+    var count = 0,max_count = 0,num_id = Number(position.id);
     var x;
     for(var i = 0;i < 4;i++){
-        
+        count = 0;
         for(var j = 0;j < 5;j++){
-            if((position + directions[i]*j) < 0 || (position + directions[i]*j) > 80) break;
-            x = document.getElementById((position + directions[i]*j)).className;
-            if(x == "none")break;
-            else if(x == Cname) num++;
+            if((num_id + directions[i]*j) < 0 || (num_id + directions[i]*j) > 80) break;
+            x = document.getElementById((num_id + directions[i]*j)).className;
+            if(x != Cname)break;
+            else if(x == Cname) count++;
         }
+        count--;
         for(var j = 0;j < 5;j++){
-            
+            if((num_id - directions[i]*j) < 0 || (num_id - directions[i]*j) > 80) break;
+            x = document.getElementById((num_id - directions[i]*j)).className;
+            if(x != Cname)break;
+            else if(x == Cname) count++;
         }
+        console.log(count)
+        max_count = (count > max_count)? count : max_count;
     }
-}*/
+    return max_count;   
+}
+
+function judge(connectted_stone){
+    if(connectted_stone >= 5){
+        var finish = document.getElementById("end");
+        finish.innerHTML = ((turn^1)? "白石の勝ち!" : "黒石の勝ち!");
+    }
+}
+
