7aecd80e036ec89e8f145549c6f8553941098ff8
tetuomi <kondo.tetta851@mail.kyutech.jp>
2019-05-16 21:21:22 +0900

    can put stone

--- Javascript/gomoku.js
+++ Javascript/gomoku.js
@@ -1,6 +1,7 @@
 const board = new Array(81);
+var turn = 0; // 0:BLACK  1:WHITE
 
-for(var i = 0;i < 81;i++){
+for(var i = 0;i < 81;i++){ //無駄なことしてるが、undefinedが入ってしまいできなっかた
     if(i%9 == 0) board[i] = "<tr><td bgcolor='#CC9966' id='No"+i+"' onclick='getID(this)'></td>";
     else if(i%9 == 8) board[i] = "<td bgcolor='#CC9966'id='No"+i+"' onclick='getID(this);'></td></tr>";
     else board[i] = "<td bgcolor='#CC9966' id='No"+i+"' onclick='getID(this);'></td>";
@@ -15,7 +16,7 @@ document.write("</center>");
 
 function getID(element) {
     var id = element.id;
-    var ok = document.getElementById("ok");
-    ok.innerHTML = id;
+    var position = document.getElementById(id);
+    position.innerHTML="<div class='circle"+turn+"'></div>";
+    turn ^= 1;
 }
-
